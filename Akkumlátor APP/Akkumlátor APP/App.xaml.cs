bf221a44b0d65c9acb3d187e84b03bd7b1c68735
NetBy <89864425+NetByOfficial@users.noreply.github.com>
2024-03-12 19:47:15 +0100

    app

---
+++ Akkumlátor APP/Akkumlátor APP/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Akkumlátor_APP
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Akkumlátor APP/Akkumlátor APP/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Akkumlátor_APP
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
